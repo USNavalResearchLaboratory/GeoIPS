c4eb563a5aedc55318428f3f91255489c8a540b1
Mindy Surratt <melinda.surratt@nrlmry.navy.mil>
2018-08-14 17:07:02 +0000

    Add gitignore from all repository locations, not just the base geoips directory. Still need to check in the gitignores, rather than .gitignore (since .gitignore is overwritten based on contents of .gitignore, and what links are present)

--- create_links.py
+++ create_links.py
@@ -99,10 +99,12 @@ for destbase in destinations:
             print gitignore_fname+' exists! Delete it if you want a new one.'
             continue
         fileobj = open(gitignore_fname,'w')
-        if os.path.exists(destbase+'/gitignore'):
-            fileobj.write('# Adding gitignore from '+destbase+'/gitignore\n')
-            for line in open(destbase+'/gitignore'):
-                fileobj.write(line.strip()+'\n')
+        # Add base gitignore information from all locations
+        for currdirname in pluginbases+[destbase]:
+            if os.path.exists(currdirname+'/gitignore'):
+                fileobj.write('# Adding gitignore from '+currdirname+'/gitignore\n')
+                for line in open(currdirname+'/gitignore'):
+                    fileobj.write(line.strip()+'\n')
     else:
         print('# Adding gitignore from '+destbase+'/gitignore\n')
 
