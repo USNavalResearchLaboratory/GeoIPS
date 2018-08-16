1f4a037563d6971d806a4cedd2187b955d957cc1
Jeremy Solbrig <jeremy.solbrig@colostate.edu>
2018-08-16 20:56:15 +0000

    Minor syntax fixes

--- geoips/geoalgs/src/extalg/extalg.py
+++ geoips/geoalgs/src/extalg/extalg.py
@@ -1,28 +1,27 @@
 #!/bin/env python
 import os
 import logging
-from IPython import embed as shell
 
 log = logging.getLogger(__name__)
 
+
 def extalg(datafile, sector, product, workdir):
     '''
-    This is a template for creating an external algorithm for operating on 
-    arbitrary data types from the datafile (registered, sectored, 
-    pre-registered...), and returning arbitrary arrays.  There must be a 
+    This is a template for creating an external algorithm for operating on
+    arbitrary data types from the datafile (registered, sectored,
+    pre-registered...), and returning arbitrary arrays.  There must be a
         geoalgs/src/extalg_plot
         geoalgs/src/extalg_coverage
-    to go with 
+    to go with
         geoalgs/src/extalg
     so the plotting and coverage checking routines know how to handle the
-    arbitrary arrays. 
-    This is a pretty useless function, but should hopefully provide a 
+    arbitrary arrays.
+    This is a pretty useless function, but should hopefully provide a
         template for more useful applications.
     '''
 
-
     '''
-    This should probably go somewhere else, but here is a template for 
+    This should probably go somewhere else, but here is a template for
     saving out the SciFile object as an hdf5.
 
     This pre-processed file will have all datatypes that were read in
@@ -34,37 +33,34 @@ def extalg(datafile, sector, product, workdir):
             write_file = True
     if write_file:
         if datafile.security_classification:
-            dirname = os.getenv('GEOIPS_OUTDIRS')+'/data/preprocessed/%s_%s'%(datafile.source_name,
-                                            datafile.security_classification.replace('/','-'))
+            dirname = os.getenv('GEOIPS_OUTDIRS') + '/data/preprocessed/{}_{}'.format(
+                datafile.source_name, datafile.security_classification.replace('/', '-'))
         else:
-            dirname = os.getenv('GEOIPS_OUTDIRS')+'/data/preprocessed/%s'%(datafile.source_name)
-        baseh5filename = '%s/%s_%s'%(dirname,
-                            sector.name,
-                            '_'.join(sorted(datafile.datasets.keys())))
+            dirname = os.getenv('GEOIPS_OUTDIRS') + '/data/preprocessed/()'.format(datafile.source_name)
+        baseh5filename = '{}/{}_{}'.format(
+            dirname, sector.name, '_'.join(sorted(datafile.datasets.keys())))
         if not os.path.isdir(dirname):
             os.makedirs(dirname)
-        h5filename = baseh5filename+'.h5'
+        h5filename = baseh5filename + '.h5'
         ii = 0
         while os.path.exists(h5filename):
-            h5filename = '%s_%03d%s'%(baseh5filename, ii, '.h5')
-            ii+=1
-        log.info('Writing out pre-sectored h5 data file: '+h5filename)
-
+            h5filename = '%s_%03d%s' % (baseh5filename, ii, '.h5')
+            ii += 1
+        log.info('Writing out pre-sectored h5 data file: ' + h5filename)
 
     '''
     Example if you want to register some data in here
     '''
     log.info('Registering datasets...')
     registered_data = datafile.register(sector.area_definition,
-            interp_method = None,
-            roi = None,
-            #required_datasets = ['B13BT'],
-            )
-
+                                        interp_method=None,
+                                        roi=None,
+                                        # required_datasets = ['B13BT'],
+                                        )
 
     '''
     This is a ridiculous process, but calling
-    extalg_config on the empty dictionary populates the 
+    extalg_config on the empty dictionary populates the
     dictionary with the appropriate variable names.
     This should be cleaned up, but this template gives us
     a starting point for specifying different sat configs
@@ -75,11 +71,10 @@ def extalg(datafile, sector, product, workdir):
     sat_config = {}
     extalg_config(sat_config)
 
-
     '''
     Here is your arbitrary outdata dictionary!!!
     You can put anything you want in here !!!!
-    Then tell <extalg>_plot how to plot each entry, 
+    Then tell <extalg>_plot how to plot each entry,
     and <extalg>_coverage how to check coverage for
     each entry!!!!
     '''
@@ -89,6 +84,6 @@ def extalg(datafile, sector, product, workdir):
         srcname = ds.source_name
         varname = sat_config[srcname]['varname'][0]
         # now set the outdata dictionary to the appropriate data array.
-        outdata[srcname+'irvar'] = ds.variables[varname]
+        outdata[srcname + 'irvar'] = ds.variables[varname]
 
     return outdata
