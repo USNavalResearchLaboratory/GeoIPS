dd40c62166ec7bc14f8960d8b4a951b560d387a4
Jeremy Solbrig <jeremy.solbrig@colostate.edu>
2018-08-16 04:59:10 +0000

    This is a test commit.  Deleted an unneeded comma.

--- geoips/about.py
+++ geoips/about.py
@@ -13,7 +13,7 @@
 __all__ = [
         "__title__", "__summary__", "__uri__", "__version__",
         "__author__", "__email__", "__license__", "__copyright__",
-        "__requires__",
+        "__requires__"
 ]
 
 
