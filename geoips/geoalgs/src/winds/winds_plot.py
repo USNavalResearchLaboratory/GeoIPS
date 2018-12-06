065af38cb2559af01f977b4e41d85f0807683b2b
Mindy Surratt <Melinda.Surratt@nrlmry.navy.mil>
2018-12-06 00:24:02 +0000

    Clean up title naming

--- geoips/geoalgs/src/winds/winds_plot.py
+++ geoips/geoalgs/src/winds/winds_plot.py
@@ -6,6 +6,7 @@ import logging
 from matplotlib import cm, colors
 import numpy as np
 from scipy.interpolate import griddata
+import re
 
 # GeoIPS Libraries
 from geoips.utils.normalize import normalize
@@ -22,10 +23,11 @@ def winds_plot(gi, imgkey=None):
 
     bgname = 'None' 
     prodname = imgkey
+    day_percent = None
     if 'BACKGROUND' in gi.image[imgkey]:
         bgfile = gi.image[imgkey]['BACKGROUND']
-        bgname = df.metadata['ds'][imgkey]['alg_channel']
-        bgvar = np.flipud(bgfile.variables[bgname])
+        bgvarname = df.metadata['ds'][imgkey]['alg_channel']
+        bgvar = np.flipud(bgfile.variables[bgvarname])
         lats = bgfile.geolocation_variables['Latitude']
         lons = bgfile.geolocation_variables['Longitude']
         sunzen = bgfile.geolocation_variables['SunZenith']
@@ -48,6 +50,22 @@ def winds_plot(gi, imgkey=None):
         enhdata.enhanceImage(config)
         bgvar = enhdata.Data
 
+        chanstr = re.sub(r"^B","Channel ",bgvarname).replace('BT',' BT').replace('Ref',' Reflectance')
+        if bgvar.wavelength:
+            bgname = '%s - %s, %s um'%(bgvar.platform_name, chanstr, bgvar.wavelength)
+        else:
+            bgname = '%s - %s'%(bgvar.platform_name, chanstr)
+        if np.ma.count(bgvar):
+            try:
+                day_percent = (1.0 * np.ma.count(bgvar[ds.day_inds]) / np.ma.count(bgvar)) * 100
+            except KeyError:
+                pass
+
+    prodname = imgkey.replace('_',' ').replace('IR','IR NOAA Winds, ').replace('WVCA','WVCA NOAA Winds, ').replace('WVCT','WVCT NOAA Winds, ').replace('WV','WV NOAA Winds, ').replace('VIS','VIS NOAA Winds, ')
+    prodname = re.sub(r"^B","Channel ",prodname).replace('BT',' BT').replace('Ref',' Reflectance')
+    if day_percent is not None:
+        prodname = '%s Product: %d'%(prodname, day_percent) + '% day'
+
 
     log.info('Setting up fig and ax for dataset: %s with bgname: %s'%(prodname, bgname))
 
@@ -56,63 +74,58 @@ def winds_plot(gi, imgkey=None):
 
     log.info('Plotting dataset: %s'%(imgkey))
 
-    if '1d' in imgkey:
-        resolution = min(gi.sector.area_info.proj4_pixel_width, gi.sector.area_info.proj4_pixel_height)
-        direction = ds.variables['direction']
-        speed = ds.variables['speed']
-        u = speed * np.cos(np.radians(direction))
-        v = speed * np.sin(np.radians(direction))
-        pres = ds.variables['pres']
-        lats = ds.variables['lats']
-        lons = ds.variables['lons']
+    resolution = min(gi.sector.area_info.proj4_pixel_width, gi.sector.area_info.proj4_pixel_height)
+    direction = ds.variables['direction']
+    speed = ds.variables['speed']
+    u = speed * np.cos(np.radians(direction))
+    v = speed * np.sin(np.radians(direction))
+    pres = ds.variables['pres']
+    lats = ds.variables['lats']
+    lons = ds.variables['lons']
 
-        from geoips.geoalgs.lib.amv_plot import downsample_winds
-        from geoips.geoalgs.lib.amv_plot import set_winds_plotting_params
+    from geoips.geoalgs.lib.amv_plot import downsample_winds
+    from geoips.geoalgs.lib.amv_plot import set_winds_plotting_params
 
-        set_winds_plotting_params(gi, speed, None, None, new_platform, new_source, prodname, bgname)
+    set_winds_plotting_params(gi, speed, None, None, new_platform, new_source, prodname, bgname)
 
-        if 'BACKGROUND' in gi.image[imgkey]:
-            log.info('Plotting background image %s'%(bgname))
-            gi.basemap.imshow(bgvar,ax=gi.axes,cmap=get_cmap(bgcmap))
-
-
-        '''
-        NOTE there appears to be an error in basemap - I had to add a try except to allow 
-        for different outputs from np.where
-        4778                 thresh = 360.-londiff_sort[-2] if nlons > 2 else 360.0 - londiff_sort[-1]
-        ***4779                 try:
-        ***4780                     itemindex = len(lonsin)-np.where(londiff>=thresh)[0][0]
-        ***4781                 except IndexError:
-        ***4782                     itemindex = len(lonsin)-np.where(londiff>=thresh)[0]
-        4783             else:
-        4784                 itemindex = 0
-        4785
-        4786             if fix_wrap_around and itemindex:
-        4787                 # check to see if cyclic (wraparound) point included
-        4788                 # if so, remove it.
-        4789                 if np.abs(lonsin[0]-lonsin[-1]) < 1.e-4:
-        4790                     hascyclic = True
-        4791                     lonsin_save = lonsin.copy()
-        4792                     lonsin = lonsin[1:]
-        /satops/geoips_externals_nrl-2.7/built/anaconda2/lib/python2.7/site-packages/mpl_toolkits/basemap/__init__.py
-        '''
+    if 'BACKGROUND' in gi.image[imgkey]:
+        log.info('Plotting background image %s'%(bgname))
+        gi.basemap.imshow(bgvar,ax=gi.axes,cmap=get_cmap(bgcmap))
 
 
-        log.info('Plotting barbs with colorbars %s'%(gi.colorbars))
+    '''
+    NOTE there appears to be an error in basemap - I had to add a try except to allow 
+    for different outputs from np.where
+    4778                 thresh = 360.-londiff_sort[-2] if nlons > 2 else 360.0 - londiff_sort[-1]
+    ***4779                 try:
+    ***4780                     itemindex = len(lonsin)-np.where(londiff>=thresh)[0][0]
+    ***4781                 except IndexError:
+    ***4782                     itemindex = len(lonsin)-np.where(londiff>=thresh)[0]
+    4783             else:
+    4784                 itemindex = 0
+    4785
+    4786             if fix_wrap_around and itemindex:
+    4787                 # check to see if cyclic (wraparound) point included
+    4788                 # if so, remove it.
+    4789                 if np.abs(lonsin[0]-lonsin[-1]) < 1.e-4:
+    4790                     hascyclic = True
+    4791                     lonsin_save = lonsin.copy()
+    4792                     lonsin = lonsin[1:]
+    /satops/geoips_externals_nrl-2.7/built/anaconda2/lib/python2.7/site-packages/mpl_toolkits/basemap/__init__.py
+    '''
 
-        gi.basemap.barbs(lons.data,lats.data,
-                        u,v,speed,
-                        ax=gi.axes,
-                        cmap=gi.colorbars[0].cmap,
-                        sizes=dict(height=0.8, spacing=0.3),
-                        norm=gi.colorbars[0].norm,
-                        linewidth=2,
-                        length=5,
-                        latlon=True)
 
+    log.info('Plotting barbs with colorbars %s'%(gi.colorbars))
 
-    else:
-        return
+    gi.basemap.barbs(lons.data,lats.data,
+                    u,v,speed,
+                    ax=gi.axes,
+                    cmap=gi.colorbars[0].cmap,
+                    sizes=dict(height=0.8, spacing=0.3),
+                    norm=gi.colorbars[0].norm,
+                    linewidth=2,
+                    length=5,
+                    latlon=True)
 
 
     if gi.is_final:
