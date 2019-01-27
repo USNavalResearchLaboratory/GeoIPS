8674456bc5423c2f6eb68307cd0a7f1a9929fee3
Mindy Surratt <Melinda.Surratt@nrlmry.navy.mil>
2019-01-27 05:46:20 +0000

    Move get_pressure_levels into winds_utils. generalize downsample_winds as thin_arrays, and remove the resoloution specific calculations (go by number of barbs per image)

--- geoips/geoalgs/src/winds/winds_utils.py
+++ geoips/geoalgs/src/winds/winds_utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import logging
+log = logging.getLogger(__name__)
 
 def kts_to_ms(speed_kts):
     # To convert to ms, multiply by 0.51444
@@ -66,40 +68,92 @@ def uv2spd(u, v):
 
     return speed, direction_deg
 
-def downsample_winds(resolution, thinvalue=None, arrs=[]):
+
+def get_pressure_levels(pres, arrays, pressure_cutoffs=[0,400,600,800,950,1014], returnInds = False, overlap = None):
+    log.info('Returning values within levels {} from array with min {} and max {}'.format(
+                        pressure_cutoffs,
+                        pres.min(),
+                        pres.max()
+                        )
+            )
+    levArrays = []
+    for arrInd in range(len(arrays)):
+        currArr = arrays[arrInd]
+        # If we're not just returning a list of indices, we must return a list of a list of arrays
+        if returnInds == False:
+            levArrays += [[]]
+        for presCutoffInd in range(len(pressure_cutoffs)-1):
+            pres1 = pressure_cutoffs[presCutoffInd]
+            pres2 = pressure_cutoffs[presCutoffInd+1]
+            if overlap is not None:
+                pres1 = pres1 - overlap
+                pres2 = pres2 + overlap
+            inds = np.ma.where(np.ma.logical_and(pres>=pres1, pres<=pres2))
+            if returnInds == True:
+                levArrays += [inds] 
+            else:
+                levArrays[arrInd] += [currArr[inds]]  
+        if returnInds == True:
+            return levArrays
+
+    return levArrays
+
+def thin_arrays(num_points, max_points=None, arrs=[], maskInds = False):
 
 
-    if not thinvalue:
-        thinvalue = 5
-        #conussizex = 1500.0
-        globalrez = 10.0
-        #Size Test determines the percentage of the sector pixel size to the CONUS pixel size
-        sizetest = float((resolution)/(globalrez))
-        percenttest = sizetest*100
-        #shell()
+#    if not max_points:
+#        thinvalue = 5
+#        #conussizex = 1500.0
+#        globalrez = 10.0
+#        #Size Test determines the percentage of the sector pixel size to the CONUS pixel size
+#        sizetest = float((resolution)/(globalrez))
+#        percenttest = sizetest*100
+#        #shell()
+#
+#
+#        if percenttest <= 75:
+#            thinvalue = int((thinvalue*sizetest)+2)
+#            #sizez = (sizez*sizetest)+6
+#        if percenttest > 105:
+#            #thinvalue = 9
+#            thinvalue = int((thinvalue*sizetest)*2)
+#            #sizez = 4
+#            #sizez = float((sizez)/(sizetest*9))
+#            #sizez = (sizez*sizetest)
+#            #bold = 1
+#            #bold = float((bold)/(sizetest*3))
+#
+#    #shell()
 
+    if max_points == None:
+        return arrs
 
-        if percenttest <= 75:
-            thinvalue = int((thinvalue*sizetest)+2)
-            #sizez = (sizez*sizetest)+6
-        if percenttest > 105:
-            #thinvalue = 9
-            thinvalue = int((thinvalue*sizetest)*2)
-            #sizez = 4
-            #sizez = float((sizez)/(sizetest*9))
-            #sizez = (sizez*sizetest)
-            #bold = 1
-            #bold = float((bold)/(sizetest*3))
-
-    #shell()
+    thinval = 1
+    if num_points > max_points:
+        thinval = int(num_points / max_points)
     retarrs = []
 
+    if thinval == 1:
+        return arrs
+
+    # If we are masking the supplied indices in place within the passed arrs, 
+    # thin as requested and mask the thinned values.
+    if maskInds is not False:
+        maskInds = (maskInds[0][0:num_points:thinval],maskInds[1][0:num_points:thinval])
+        for arr in arrs:
+            arr.mask = True
+            arr.mask[maskInds] = False
+        return arrs
+
+    # If we are returning a smaller array, thin and return smaller arrays
     try:
         for arr in arrs:
-            retarrs += [arr[::thinvalue,::thinvalue]]
+            retarrs += [arr[::thinval,::thinval]]
 			
     except IndexError:
         for arr in arrs:
-            retarrs += [arr[::thinvalue]]
+            retarrs += [arr[::thinval]]
+
+
 
     return retarrs
