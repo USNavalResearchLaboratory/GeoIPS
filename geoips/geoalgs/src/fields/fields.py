94c33c766961e2c43f278e4147ebe0c27a0bb374
Mindy Surratt <melinda.surratt@nrlmry.navy.mil>
2019-02-22 20:54:15 +0000

    Passing through all variables to plot

--- geoips/geoalgs/src/fields/fields.py
+++ geoips/geoalgs/src/fields/fields.py
@@ -7,9 +7,17 @@ log = logging.getLogger(__name__)
 def fields(datafile, sector, product, workdir):
     '''
     '''
-    #from IPython import embed as shell
-    #shell()
+    if sector.name not in datafile.datasets.keys():
+        return [], None
 
-    #return finaldata, None
-    return [], None
+    ds = datafile.datasets[sector.name]
+
+    finaldata = {}
+    for varname in ds.variables.keys():
+        finaldata['{0}_{1}'.format(varname, sector.name)] = datafile
+        if 'uwind' in varname:
+            lev = int(varname[-4:])
+            finaldata['Winds_{0:d}_{1}'.format(lev, sector.name)] = datafile
+
+    return finaldata, None
             
